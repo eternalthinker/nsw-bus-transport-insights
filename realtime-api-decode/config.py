51d6dd24ad54706ca480f4427bc23f63b83e092d
Rahul Anand <rahulanand.inbox@gmail.com>
2018-05-06 11:10:23 +1000

    Added mapping txt files

--- realtime-api-decode/config.py
+++ realtime-api-decode/config.py
@@ -1,16 +1,23 @@
 config = {
-	"endpoints": [
-		{
+	"endpoints": {
+		"vehiclepos": {
 			"endpoint": "https://api.transport.nsw.gov.au/v1/gtfs/vehiclepos/buses",
 			"filename": "vehiclepos"
 		},
-		{
+		"alerts": {
 			"endpoint": "https://api.transport.nsw.gov.au/v1/gtfs/alerts/buses",
 			"filename": "alerts"
 		},
-		{
+		"trip_updates": {
 			"endpoint": "https://api.transport.nsw.gov.au/v1/gtfs/realtime/buses",
 			"filename": "trip_updates"
 		}
+	},
+	"routes": [
+		"2441_391",
+		"2441_393",
+		"2441_395",
+		"2438_M10",
+		"2441_891"
 	]
 }
